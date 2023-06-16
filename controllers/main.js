2f4f34dbaaf78fb2083d7a0d62498af9ea3947d5
PGiang <vophonggiang0205@gmail.com>
2023-06-16 17:31:42 +0700

    update

--- controllers/main.js
+++ controllers/main.js
@@ -1,52 +1,70 @@
-import Api from "./../utils/api.js";
+import Api from './../utils/api.js';
 const api = new Api();
-// GetEle:
-const getEle = (id) => document.getElementById(id)
 
+// Get element by ID
+const getEle = (id) => document.getElementById(id);
 
-// Call API get Data from dressingroom:
+// Call API to get data from dressingroom
 function renderData() {
     api.callApi("dressingroom", "GET", null)
         .then((rs) => {
-            // run renderUI of the page
-            renderUI(rs.data)
+            renderNavTab(rs.data);
+            let tabPanes = queryTabPanes();
+            renderProduct(rs.data, tabPanes);
         })
         .catch((error) => {
-            console.log(error)
-        })
-};
-renderData()
+            console.log(error);
+        });
+}
 
-function renderUI(data) {
+renderData();
+
+const navTab = getEle("navTab");
+const tabContent = getEle("tab-content");
+
+function renderNavTab(data) {
     let content = "";
-    console.log(data)
+    let contenttab = "";
     if (data && data.length > 0) {
-        data.forEach((product) => {
-            content +=
-                `
-                        <div class="col-lg-3 col-md-6 my-3">
-                            <div class="card product text-black h-100">
-                            <div class="card__Overlay">
-                                        <div class="card__OverlayHeader">
-                                        ${product.name}
-                                        </div>
-                                        <div class="card__OverlayBody">
-                                            <a href="">Click here for more details</a>
-                                        </div>
-                                        <div class = "card__OverlayFooter">
-                                        <button type="button" class="" id="btnAdd" onclick="btnAddToCart(${product.id})" >Add to Cart</button>
-                                        </div>
-                            </div>
-                                <img src="${product.img}" class="" alt="${product.name}">
-                                <div class="card-body">
-                                <p>${product.type}</p>
-                                <h5 class="card-title">${product.name}</h5>
-                                <p class="card-text">Price: ${product.price}</p>
-                                </div>
-                            </div>
-                        </div>
-                            `;
-        })
-        getEle("productList").innerHTML = content;
+        data.forEach((navArr) => {
+            let navbar = navArr.navPills;
+            navbar.forEach((navItem) => {
+                content += `<li class="nav-item">
+                                <a class="nav-link" data-toggle="tab" href="#${navItem.type}" role="tab" aria-controls="${navItem.tabName}">${navItem.showName}</a>
+                            </li>`;
+                contenttab += `<div class="tab-pane d-flex" id="${navItem.type}" role="" aria-labelledby="${navItem.type}"></div>`
+            });
+            tabContent.innerHTML = contenttab;
+            navTab.innerHTML = content;
+        });
     }
 }
+
+function queryTabPanes() {
+    return document.querySelectorAll('.tab-pane');
+}
+function renderProduct(data, panes) {
+    let contentProduct = "";
+    // Perform actions specific to the active tab
+    panes.forEach((pane) => {
+
+        if (data && data.length > 0) {
+            data.forEach((tabPanesArr) => {
+                let tabPanes = tabPanesArr.tabPanes;
+                console.log(pane.id)
+                tabPanes.forEach((tab) => {
+                    if (tab.type === pane.id) {
+
+                        contentProduct += `<div id="" class = "d-flex flex-column" role="" aria-labelledby="${tab.type}">
+                    <img src="${tab.imgSrc_jpg}">
+                    ${tab.name}
+                    </div>`;
+                        pane.innerHTML = contentProduct;
+                    }
+                });
+            });
+        }
+    });
+}
+
+
